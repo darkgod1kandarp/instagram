b3e1cf83486f3f8353f2e3fd5f4d121596625c1b
Heet Vakharia <heet1476@gmail.com>
2021-01-19 15:22:35 +0530

    Added Form For Changing Password

--- The-Circle-master/src/App.js
+++ The-Circle-master/src/App.js
@@ -10,7 +10,7 @@ import Overview from "./pages/Overview.pages";
 import SignupFormComp from "./pages/Signup.pages";
 import SigninFormComp from "./pages/Signin.pages";
 import ForgetPasswordFormComp from "./pages/Forget.pages";
-import FormSecondary from "./components/FormSecondary.component"
+import FormSecondary from "./components/FormSecondary.component";
 import { useState, useEffect } from "react";
 import Explore from "./pages/Explore.pages";
 import DashBoard from "./App";
@@ -32,28 +32,32 @@ function App() {
     //   history.push("/Signup");
     //   console.log(history)
     // }
-    
-    
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const MenuBar = () => {
-    if(user.id) {
-      console.log("hello")
-      return <Sidebar setUser={setUser} />
-   }
-   else{
-     return <div></div>
-   }
-  }
+    if (user.username) {
+      console.log("hello");
+      return <Sidebar setUser={setUser} />;
+    } else {
+      return <div></div>;
+    }
+  };
   return (
     <div>
-      <MenuBar/>
+      <MenuBar />
       <Switch>
         <Route path="/explore" component={Explore} />
-        <Route exact path="/Signup" render={() => <SignupFormComp setUser={setUser} history={history}/>} />
+        <Route
+          exact
+          path="/Signup"
+          render={() => <SignupFormComp setUser={setUser} history={history} />}
+        />
         <Route path="/SignIn" component={SigninFormComp} />
         <Route path="/Forgot" component={ForgetPasswordFormComp} />
-        <Route path="/getInfo" render={() => <FormSecondary user={user} history={history}/>}/>
+        <Route
+          path="/getInfo"
+          render={() => <FormSecondary user={user} history={history} />}
+        />
         {/* <Route path="/DashBoard" component={DashBoard}/> */}
         <Route path="/DashBoard" component={DashBoard}></Route>
         <Route path="/" render={() => <CrushSelection user={user} />}></Route>
